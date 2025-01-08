c936efa9286e793a72b078fb83ecc0fdb0b8dc2e
Tania Salvatella <tsalvatellap@gmail.com>
2025-01-08 11:38:16 +0100

    checkbox

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const btnFilter = document.querySelector(".js-btn-filter");
 const list = document.querySelector(".js-list");
 const span = document.querySelector(".js-span");
 
+
+//Tenemos que crear una funcion que modifique el array para cambiar las tareas a completas o no. Un splice
 const tasks = [
     { name: "Recoger setas en el campo", completed: true, id: 1 },
     { name: "Comprar pilas", completed: true, id: 2 },
@@ -18,16 +20,25 @@ const tasks = [
   ];
 
   function renderTask () {
+
     list.innerHTML = "";
     for (const task of tasks) {
     
     if (task.completed === true) {
-      list.innerHTML += `<li class="tachado"> ${task.name}</li>`;  
+      list.innerHTML += `<li class="tachado"><input type="checkbox" checked /> ${task.name}</li>`; 
     } else {
-        list.innerHTML += `<li> ${task.name}</li>`;  
+        list.innerHTML += `<li><input type="checkbox"/> ${task.name}</li>`;  
     }
   }
 };
+
+//tenemos que escuchar el evento en la ul
+function handleCheckboxCompleted (){
+
+   
+}
+
+
 
 function handleClickAdd (ev) {
     ev.preventDefault ();
@@ -44,3 +55,20 @@ function handleClickAdd (ev) {
 
   renderTask ();
   btnAdd.addEventListener("click", handleClickAdd);
+
+
+
+
+  //Ejercicio  2.12 
+
+
+  const handleClickList = (event) => {
+    const taskId = parseInt(event.target.id); // Obtengo el id del checkbox clickado por la usuaria
+    if (!taskId) return; // Si no ha pulsado en el checkbox, no queremos hacer nada y salimos de la función
+  
+    // Busca la tarea que tenga el id `taskId` en el array `tasks`
+    // Una vez que has obtenido la tarea, actualiza la propiedad `completed`
+    // Pinta de nuevo las tareas en el html
+  };
+  
+  list.addEventListener("click", handleClickList);
